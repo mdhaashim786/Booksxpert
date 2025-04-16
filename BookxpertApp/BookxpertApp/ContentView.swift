6e16fdb3c47ef4e84ebe6bbef7d5617ff6eff555
mdhaashim786 <mdhaashim786@gmail.com>
2025-04-17 00:32:00 +0530

    Implement the google signin functionality

--- BookxpertApp/BookxpertApp/ContentView.swift
+++ BookxpertApp/BookxpertApp/ContentView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 import CoreData
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+import GoogleSignInSwift
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -15,30 +19,79 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    @State private var isSignedIn = false
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        if isSignedIn {
+            NavigationView {
+                List {
+                    ForEach(items) { item in
+                        NavigationLink {
+                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        } label: {
+                            Text(item.timestamp!, formatter: itemFormatter)
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    ToolbarItem {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
+                Text("Select an item")
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+        } else {
+            GoogleSignInButton {
+                signInWithGoogle()
+
+            }
+            .frame(width: 200, height: 50)
+        }
+    }
+    
+    func signInWithGoogle() {
+
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let presentingVC = windowScene.windows.first?.rootViewController else {
+            print("Unable to get root view controller.")
+            return
+        }
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingVC) { result, error  in
+            if let error = error {
+                print("Google Sign-In error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString else {
+                return
+            }
+            
+            let accessToken = user.accessToken.tokenString
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Firebase Sign-In error: \(error.localizedDescription)")
+                } else {
+                    print("Successfully signed in")
+                    isSignedIn = true
+                    print(user.userID)
+                    print(user.profile?.email)
+                    print(user.profile?.name)
+                    print(user.profile?.givenName)
+                    print(user.profile?.familyName)
+                    print(user.profile?.imageURL(withDimension: 200)?.absoluteString)
                 }
             }
-            Text("Select an item")
         }
     }
 
