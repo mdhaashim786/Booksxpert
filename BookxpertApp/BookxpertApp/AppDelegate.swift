bf8c91b47e5616db1bf17c6c89a1c96f9f1df9d3
mdhaashim786 <mdhaashim786@gmail.com>
2025-04-16 21:55:48 +0530

    Setup configuration required for google signin

--- BookxpertApp/BookxpertApp/AppDelegate.swift
+++ BookxpertApp/BookxpertApp/AppDelegate.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         FirebaseApp.configure()
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return false }
+        let config = GIDConfiguration(clientID: clientID)
+        
+        GIDSignIn.sharedInstance.configuration =  config
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
