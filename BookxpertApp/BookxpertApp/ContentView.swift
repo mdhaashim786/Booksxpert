2de20a17fc241bfe61f2b5180db2668cd9320832
mdhaashim786 <mdhaashim786@gmail.com>
2025-04-17 22:27:09 +0530

    Keep the UI in the View and move the logic to the view model

--- BookxpertApp/BookxpertApp/ContentView.swift
+++ BookxpertApp/BookxpertApp/ContentView.swift
@@ -6,133 +6,40 @@
 //
 
 import SwiftUI
-import CoreData
-import FirebaseCore
-import FirebaseAuth
-import GoogleSignIn
 import GoogleSignInSwift
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-    @State private var isSignedIn = false
-
-    var body: some View {
-        if isSignedIn {
-            NavigationView {
-                List {
-                    ForEach(items) { item in
-                        NavigationLink {
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                        } label: {
-                            Text(item.timestamp!, formatter: itemFormatter)
-                        }
-                    }
-                    .onDelete(perform: deleteItems)
-                }
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        EditButton()
-                    }
-                    ToolbarItem {
-                        Button(action: addItem) {
-                            Label("Add Item", systemImage: "plus")
-                        }
-                    }
-                }
-                Text("Select an item")
-            }
-        } else {
-            GoogleSignInButton {
-                signInWithGoogle()
-
-            }
-            .frame(width: 200, height: 50)
-        }
-    }
+    @StateObject var viewModel = LoginViewModel()
     
-    func signInWithGoogle() {
-
-        
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let presentingVC = windowScene.windows.first?.rootViewController else {
-            print("Unable to get root view controller.")
-            return
-        }
-        
-        GIDSignIn.sharedInstance.signIn(withPresenting: presentingVC) { result, error  in
-            if let error = error {
-                print("Google Sign-In error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let user = result?.user,
-                  let idToken = user.idToken?.tokenString else {
-                return
-            }
-            
-            let accessToken = user.accessToken.tokenString
-            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
-            
-            Auth.auth().signIn(with: credential) { authResult, error in
-                if let error = error {
-                    print("Firebase Sign-In error: \(error.localizedDescription)")
-                } else {
-                    print("Successfully signed in")
-                    isSignedIn = true
-                    print(user.userID)
-                    print(user.profile?.email)
-                    print(user.profile?.name)
-                    print(user.profile?.givenName)
-                    print(user.profile?.familyName)
-                    print(user.profile?.imageURL(withDimension: 200)?.absoluteString)
+    var body: some View {
+        VStack {
+            if viewModel.isSignedIn {
+                //NavigationView {
+                VStack {
+                    Text("Welcome to the app")
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        Text("Signout")
+                    })
+                    
                 }
+            } else {
+                GoogleSignInButton {
+                    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                       let presentingVC = windowScene.windows.first?.rootViewController  {
+                        viewModel.signInWithGoogle(presentingVC: presentingVC)
+                    }
+                }
+                .frame(width: 200, height: 50)
             }
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        .onAppear {
+            viewModel.setContext(viewContext)
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
---
+++ BookxpertApp/BookxpertApp/LoginViewModel.swift
@@ -0,0 +1,127 @@
+//
+//  LoginViewModel.swift
+//  BookxpertApp
+//
+//  Created by mhaashim on 17/04/25.
+//
+
+import SwiftUI
+import CoreData
+import FirebaseAuth
+import GoogleSignIn
+
+class LoginViewModel: ObservableObject {
+    
+    // Just added to store userProfile
+    @Published var userprofile: UserProfile?
+    @Published var isSignedIn: Bool = false
+    
+    private var context: NSManagedObjectContext?
+    
+    func setContext(_ context: NSManagedObjectContext) {
+        self.context = context
+        self.isSignedIn = Auth.auth().currentUser != nil
+        fetchProfile()
+    }
+    
+    func signInWithGoogle(presentingVC: UIViewController?) {
+        guard let presentingVC = presentingVC else {
+            print("Missing presenting view controller")
+            return
+        }
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingVC) { result, error  in
+            if let error = error {
+                print("Google Sign-In error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString else {
+                return
+            }
+            
+            let accessToken = user.accessToken.tokenString
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Firebase Sign-In error: \(error.localizedDescription)")
+                } else {
+                    print("Successfully signed in")
+                    
+                    self.isSignedIn = true
+                    self.saveData(userData: user)
+                }
+            }
+        }
+    }
+    
+    func signOut() {
+        
+        do {
+            try Auth.auth().signOut()
+            print("Signed out from Firebase")
+        } catch {
+            print("Firebase sign-out error: \(error.localizedDescription)")
+        }
+        
+        GIDSignIn.sharedInstance.signOut()
+        
+        guard let context = context else { return }
+        
+        let fetchRequest: NSFetchRequest<UserProfile> = UserProfile.fetchRequest()
+        
+        do {
+            let users = try context.fetch(fetchRequest)
+            users.forEach { context.delete($0) }
+            try context.save()
+            print("Deleted user from Core Data")
+        } catch {
+            print("Failed to delete user from Core Data: \(error.localizedDescription)")
+        }
+        
+        // 4. Update UI state
+        DispatchQueue.main.async {
+            self.isSignedIn = false
+        }
+    }
+    
+    private func saveData(userData: GIDGoogleUser) {
+        withAnimation {
+            if let context = context {
+                let userProfile = UserProfile(context: context)
+                userProfile.email = userData.profile?.email
+                userProfile.name = userData.profile?.name
+                userProfile.userId = UUID(uuidString: userData.userID ?? "")
+                userProfile.givenName = userData.profile?.givenName
+                userProfile.imageUrl = userData.profile?.imageURL(withDimension: 200)?.absoluteString
+                
+                self.userprofile = userProfile
+                do {
+                    try context.save()
+                } catch {
+                    print("Error occured \(error)")
+                }
+            }
+        }
+    }
+    
+    
+    func fetchProfile() {
+        guard let context else {
+            print("Context not set")
+            return
+        }
+        
+        let request: NSFetchRequest<UserProfile> = UserProfile.fetchRequest()
+        request.sortDescriptors = []
+        
+        do {
+            userprofile = try context.fetch(request).first
+        } catch {
+            print("Failed to fetch users: \(error)")
+        }
+    }
+    
+}
